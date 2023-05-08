deada5edf1b90763c5059174d131ecd8b2a2a9c3
samuelemadrigali <samuele.madrigali@gmail.com>
2023-05-08 13:02:14 +0200

    add: objects

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
 /*------------------
     VARIABLES
 -------------------*/
-const lists = ["Fare la spesa", "Preparare il pranzo", "Portare fuori il cane"];
+const lists = [
+  {
+    title: "Fare la spesa",
+    completed: false,
+  },
+  {
+    title: "Preparare il pranzo",
+    completed: false,
+  },
+  {
+    title: "Portare fuori il cane",
+    completed: true,
+  },
+];
 const form = document.querySelector("#form-todo");
 const listTodoElement = document.querySelector("#list-todo");
 const inputElement = document.querySelector("#new-todo-field");
 /*------------------
     FUNCTIONS
 -------------------*/
+function onClickTodo() {
+  lists[this.id].completed = !lists[this.id].completed;
+  this.classList.toggle("done");
+}
+
 function renderList() {
   listTodoElement.innerHTML = "";
-  lists.forEach(function (elm) {
+  lists.forEach(function (elm, index) {
     const listItem = document.createElement("li");
-    listItem.textContent = elm;
+    listItem.textContent = elm.title;
+    listItem.id = index;
+    if (elm.completed) {
+      listItem.classList.add("done");
+    }
+    listItem.addEventListener("click", onClickTodo);
     listTodoElement.append(listItem);
   });
 }
@@ -27,7 +50,10 @@ renderList();
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   // Aggiungo un listItem alla lista
-  lists.push(inputElement.value);
+  lists.push({
+    title: inputElement.value,
+    completed: false,
+  });
   renderList();
   // Resetto il valore del campo input
   form.reset();
